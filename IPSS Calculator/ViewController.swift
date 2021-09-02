afde8b1c83641116b272f2a2c5bd5164d5a43d4d
All_WE <all_we@icloud.com>
2021-09-02 21:21:08 +0700

    Initial Commit

---
+++ IPSS Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IPSS Calculator
+//
+//  Created by Afriza Alafwandy Winata on 02/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
